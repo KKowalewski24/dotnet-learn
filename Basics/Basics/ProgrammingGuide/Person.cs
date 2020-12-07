8f3a02d3c3b8df476ed639deeb95bc053123dab8
Kamil Kowalewski <szamil24@gmail.com>
2020-12-07 19:02:13 +0100

    Add read write to file + attributes

---
+++ Basics/Basics/ProgrammingGuide/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Basics.ProgrammingGuide {
+
+    [Serializable]
+    public class Person {
+
+        /*------------------------ FIELDS REGION ------------------------*/
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+
+        /*------------------------ METHODS REGION ------------------------*/
+        public Person(string firstName, string lastName) {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public override string ToString() {
+            return $"{nameof(FirstName)}: {FirstName}, " +
+                   $"{nameof(LastName)}: {LastName}";
+        }
+
+    }
+
+}
---
+++ Basics/Basics/ProgrammingGuide/ProgrammingGuideMain.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Basics.ProgrammingGuide {
+
+    public class ProgrammingGuideMain {
+
+        /*------------------------ FIELDS REGION ------------------------*/
+
+        /*------------------------ METHODS REGION ------------------------*/
+        public void Main() {
+            WriteReadToFile();
+            PrintAuthorInfo(typeof(SampleClass));
+        }
+
+        private void WriteReadToFile() {
+            Person person = new Person("first", "last");
+            const string filename = "person.txt";
+
+            if (File.Exists(filename)) {
+                File.Delete(filename);
+            }
+
+            IFormatter formatter = new BinaryFormatter();
+
+            using (FileStream stream = new FileStream(filename, FileMode.Create)) {
+                formatter.Serialize(stream, person);
+            }
+
+            Person person2;
+            using (FileStream stream = new FileStream(filename, FileMode.Open, FileAccess.Read)) {
+                person2 = (Person) formatter.Deserialize(stream);
+            }
+
+            Console.WriteLine(person2);
+        }
+
+        private void PrintAuthorInfo(Type type) {
+            Attribute.GetCustomAttributes(type).ToList().ForEach((attribute) => {
+                if (attribute is AuthorAttribute authorAttribute) {
+                    Console.WriteLine($"{authorAttribute.Name}, {authorAttribute.Version}");
+                }
+            });
+
+        }
+
+    }
+
+}

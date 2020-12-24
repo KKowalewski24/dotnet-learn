8c19378cd010ff9b1b8630b123f3455fe4d9f816
Kamil Kowalewski <szamil24@gmail.com>
2020-12-24 11:46:15 +0100

    Add Date usage

--- Basics/Basics/ProgrammingGuide/Properties/PropertiesMain.cs
+++ Basics/Basics/ProgrammingGuide/Properties/PropertiesMain.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Basics.ProgrammingGuide.Properties {
 
@@ -17,6 +18,13 @@ namespace Basics.ProgrammingGuide.Properties {
 
             SaleItem item = new SaleItem("Shoes", 19.95m);
             Console.WriteLine($"{item.Name}: sells for {item.Price:C2}");
+
+            Date date = new Date();
+            IList<int> monthsNumber = date.MonthsNumber;
+            Console.WriteLine(monthsNumber.Count);
+            monthsNumber.Add(152);
+            Console.WriteLine(monthsNumber.Count);
+            Console.WriteLine(date.MonthsNumber.Count);
         }
 
     }
